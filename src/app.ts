fe8bac1dee3b73ccfbeea492be1eb31d525f500b
unknown <mattavzzz@gmail.com>
2022-03-16 22:39:16 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import passportInitialize from "./passportConfig";
 
 const app = express();
 config();
+app.set("trust-proxy", 1)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -24,7 +25,7 @@ app.use(
   session({
     secret: "secretcode",
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
       sameSite: "none",
       secure: true,
